34d45e8911228d968909704b5dd252fc0edc5af8
RichardHsiung <xiongxing@eloancn.com>
2017-07-26 12:08:34 +0800

    for all

--- macd_check.py
+++ macd_check.py
@@ -10,22 +10,20 @@ from matplotlib.pylab import date2num
 from matplotlib.dates import DateFormatter, WeekdayLocator, DayLocator, MONDAY
 from matplotlib.finance import candlestick_ohlc
 
-def _get_stock_data(code, start_time, end_time):
+def _get_stock_data(code, start_time, end_time, autype):
 
-    stock_data = ts.get_k_data(code, start=start_time, end=end_time, autype=None)
-    #stock_data = stock_data.sort_index(0)
-    #stock_data.index = pd.to_datetime(stock_data.index)
-    print(stock_data)
+    stock_data = ts.get_k_data(code, start=start_time, end=end_time, autype=autype)
     return stock_data
 
 
-def macd_check(code, start_time, end_time, short, long):
+def macd_check(code, start_time, end_time, autype, short_day, long_day):
     sd = 0.05  #width of the region
-    macd_all = _get_stock_data(code, start_time, end_time)
+    macd_all = _get_stock_data(code, start_time, end_time, autype)
 
-    macd_all["short"] = np.round(macd_all["close"].rolling(window = int(short), center = False).mean(), 2)
-    macd_all["long"] = np.round(macd_all["close"].rolling(window = int(long), center = False).mean(), 2)
+    macd_all["short"] = np.round(macd_all["close"].rolling(window = int(short_day), center = False).mean(), 2)
+    macd_all["long"] = np.round(macd_all["close"].rolling(window = int(long_day), center = False).mean(), 2)
 
+    # s-l > long * sd  buy the stock
     macd_all["s-l"] = macd_all["short"] - macd_all["long"]
     macd_all['Regime'] = np.where(macd_all['s-l'] > macd_all["long"] * sd, 1, 0)
     print(macd_all['Regime'].value_counts())
@@ -42,7 +40,8 @@ if __name__ == "__main__":
     start_time="2008-01-01"
     #end_time=datetime.datetime.now().strftime('%Y-%m-%d')
     end_time = "2015-04-23"
+    autype = None  #默认不复权
     code = sys.argv[1]
     mask = "000000"
     code = mask[len(code) - 1: -1] + code
-    macd_check(code, start_time, end_time, 20, 120)
+    macd_check(code, start_time, end_time, autype, 20, 120)
